6201d22f424f90a1fa3143da98f3801d31dc2d13
Balasubramanian Srinivasan <bala41288@gmail.com>
2021-03-25 08:49:00 +0530

    Feature to check if the file already exists before taking backup

--- app.js
+++ app.js
@@ -1,36 +1,52 @@
+const fs = require('fs');
+const { spawn } = require('child_process');
 const config = require('./config.json');
-spawn = require('child_process').spawn
 
-const today = new Date();
-const dd = today.getDate();
+let today = new Date();
+let dd = today.getDate();
 
-const mm = today.getMonth()+1; 
+let mm = today.getMonth() + 1;
 const yyyy = today.getFullYear();
-if(dd<10) 
-{
-    dd='0'+dd;
-} 
+if (dd < 10) {
+  dd = `0${dd}`;
+}
 
-if(mm<10) 
-{
-    mm='0'+mm;
-} 
-today = dd+'-'+mm+'-'+yyyy;
+if (mm < 10) {
+  mm = `0${mm}`;
+}
+today = `${dd}-${mm}-${yyyy}`;
 
-let backupProcess = spawn('mongodump', [
-    `-d=${config.database}`,
-    `--archive="${config.backupLocation}${config.database}-${today}.archive"`,
-    '--gzip',
-    `-u="${config.username}"`,
-    `-p="${config.password}"`,
-    '--authenticationDatabase=admin'
-    ]);
+const backupProcess = spawn('mongodump', [
+  `-d=${config.database}`,
+  `--archive="${config.backupLocation}${config.database}-${today}.archive"`,
+  '--gzip',
+  `-u="${config.username}"`,
+  `-p="${config.password}"`,
+  '--authenticationDatabase=admin',
+]);
 
-backupProcess.on('exit', (code, signal) => {
-    if(code) 
-        console.log('Backup process exited with code ', code);
-    else if (signal)
-        console.error('Backup process was killed with signal ', signal);
-    else 
-        console.log('Successfully backedup the database')
-});
+const init = async () => {
+  try {
+    if (fs.existsSync(`${config.backupLocation}${config.database}-${today}.archive"`)) {
+      // eslint-disable-next-line no-console
+      console.log('The file exists already.');
+      await fs.unlinkSync(`${config.backupLocation}${config.database}-${today}.archive"`);
+      // eslint-disable-next-line no-console
+      console.log('The file has been removed successfully. The backup process will start.');
+      backupProcess.on('exit', (code, signal) => {
+        // eslint-disable-next-line no-console
+        if (code) { console.log('Backup process exited with code ', code); } else if (signal) { console.error('Backup process was killed with signal ', signal); } else { console.log('Successfully backed up the database'); }
+      });
+    } else {
+      backupProcess.on('exit', (code, signal) => {
+        // eslint-disable-next-line no-console
+        if (code) { console.log('Backup process exited with code ', code); } else if (signal) { console.error('Backup process was killed with signal ', signal); } else { console.log('Successfully backed up the database'); }
+      });
+    }
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(err);
+  }
+};
+
+init();
